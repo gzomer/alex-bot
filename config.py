e96f8af4166eadf952d8e2302a470ea52f25ddce
Gustavo Zomer <gustavozomer@gmail.com>
2020-04-13 18:40:36 +0100

    Remove Trello config

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ BOT_FILES_BUCKET_NAME = ''
 # Replace with the Kendra index
 KENDRA_COMPANY_INDEX = ''
 
-# Replace with your Trello config. You can use this one, which I have created for a demo account
+# Replace with your Trello config. 
 AUTH_TRELLO = {
-    'key': '7f55ab53aa45b172d5d4c5d31697f9ca',
-    'token': '2677eddf219e5f5fe5d7cf29bc4b32afebbb453755a2fdd4c8ddd4557abba00b'    
+    'key': '',
+    'token': ''    
 }
 
 # Replace with the Board ID to where the tasks will be created
-TRELLO_BOARD_ID = 'gPd36jmG'
+TRELLO_BOARD_ID = ''
 
