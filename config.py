66d6cf8246dbbee01c831aac678a125f5f5a3a64
Gustavo Zomer <gustavozomer@gmail.com>
2020-04-13 16:50:38 +0100

    Add config file

---
+++ config.py
@@ -0,0 +1,18 @@
+# Replace with your AWS Account ID
+ACCOUNT_ID = ''
+
+# Replace with the S3 bucket to store the bot files
+BOT_FILES_BUCKET_NAME = ''
+
+# Replace with the Kendra index
+KENDRA_COMPANY_INDEX = ''
+
+# Replace with your Trello config. You can use this one, which I have created for a demo account
+AUTH_TRELLO = {
+    'key': '7f55ab53aa45b172d5d4c5d31697f9ca',
+    'token': '2677eddf219e5f5fe5d7cf29bc4b32afebbb453755a2fdd4c8ddd4557abba00b'    
+}
+
+# Replace with the Board ID to where the tasks will be created
+TRELLO_BOARD_ID = 'gPd36jmG'
+
